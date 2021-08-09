b6d26cb17d2468344a122a860fe9ac46081dc80f
2av - Martin Schmitt <ms@2av.de>
2021-08-09 16:17:03 +0200

    linter fixes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
-import router from "./router"
+import router from './router'
 import App from './App.vue'
 
 // tailwind css
 import './index.css'
-
-
 
 const app = createApp(App)
 app.use(router)
--- src/router.js
+++ src/router.js
@@ -1,15 +1,15 @@
-import { createRouter, createWebHashHistory} from "vue-router"
-import Home from "./components/Home.vue"
-import About from "./components/About.vue"
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+    { path: '/', component: Home },
+    { path: '/about', component: About },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes, 
+    history: createWebHashHistory(),
+    routes,
 })
 
 export default router
