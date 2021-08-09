50deab2b4e352335654bf68cf8dbd41bd6c00595
Thomas Geissl <tg@2av.de>
2021-08-09 17:35:37 +0200

    moved router to its own directory, renamed index.js files

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from './components/Home.vue'
-import About from './components/About.vue'
+import Home from '../components/Home.vue'
+import About from '../components/About.vue'
 
 const routes = [
     { path: '/', component: Home },
