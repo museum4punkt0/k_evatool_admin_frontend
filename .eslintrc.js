5244f74048fea8c6fb981ee4e90e8bddb481d2a9
2av - Martin Schmitt <ms@2av.de>
2021-08-09 16:14:33 +0200

    deleted .idea eslint fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,21 @@ module.exports = {
     env: {
         node: true,
     },
-
-    extends: [
-        'plugin:vue/vue3-recommended',
-    ],
-
     parserOptions: {
         parser: 'babel-eslint',
         ecmaVersion: 2020,
     },
-
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'vue/no-unused-components':
-            'off',
+        'vue/no-unused-components': 'off',
         'vue/no-v-html': 'off',
         'vue/no-deprecated-filter': 'off',
     },
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/prettier',
+    ],
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// tailwind css
 import './index.css'
 
 createApp(App).mount('#app')
