482764b86e9c36f7e4aebb7d730bdee4eda91d5d
Thomas Geissl <tg@2av.de>
2021-09-02 18:30:06 +0200

    added leader line

--- src/components/NodeEditor/NodeEditor.vue
+++ src/components/NodeEditor/NodeEditor.vue
@@ -1,7 +1,10 @@
 <template>
     <Container>node editor</Container>
+    <button @click="drawConnections">test draw connection</button>
+    <button @click="updateConnections">test update connection</button>
     <Node
         v-for="node in nodes"
+        :id="'evtool_node_' + node.id"
         :key="node.title"
         :ref="
             (el) => {
@@ -15,10 +18,10 @@
 </template>
 
 <script>
-import { ref, watch, onBeforeUpdate } from 'vue'
+import { ref, watch, onBeforeUpdate, onUnmounted } from 'vue'
 import styled from 'vue3-styled-components'
 import Node from './Node.vue'
-import LeaderLine from 'leader-line-vue'
+import LeaderLine from 'vue3-leaderline'
 const Container = styled.div`
     overflow: scroll;
     background-color: CadetBlue;
@@ -36,7 +39,12 @@ export default {
         },
     },
     setup(props) {
+        console.log(LeaderLine)
         const nodeRefs = ref({})
+        const connectionElements = ref([])
+        props.nodes.forEach((node) => {
+            nodeRefs.value[node.id] = null
+        })
         const highlightInlets = ref(false)
         const highlightOutlets = ref(false)
         watch(
@@ -45,10 +53,12 @@ export default {
                 console.log('nodes changed', nodes)
             },
         )
-        // LeaderLine.setLine(this.$refs['17'], this.$refs['2'])
-        onBeforeUpdate(() => {
-            console.log(nodeRefs)
-            nodeRefs.value = []
+        onBeforeUpdate(() => {})
+        onUnmounted(() => {
+            connectionElements.value.forEach((connectionElement) => {
+                // document.removeChild(connectionElement)
+                connectionElement.remove()
+            })
         })
         return {
             highlightInlets,
@@ -58,7 +68,33 @@ export default {
                 const outlets = [{ name: 'next', value: node.nextStepId }]
                 return outlets
             },
+            nodeRefs,
+            connectionElements,
             connections: [{ from: null, to: null }],
+            drawConnections: () => {
+                console.log(LeaderLine)
+                const keys = Object.keys(nodeRefs.value)
+                console.log(keys)
+                if (keys.length > 1) {
+                    console.log(
+                        // nodeRefs.value[keys[0]],
+                        // nodeRefs.value[keys[1]].get(),
+                        document.getElementById('evtool_node_' + keys[0]),
+                        document.getElementById('evtool_node_' + keys[1]),
+                    )
+                    const line = new LeaderLine(
+                        document.getElementById('evtool_node_' + keys[0]),
+                        document.getElementById('evtool_node_' + keys[1]),
+                        { color: 'red', size: 8 },
+                    )
+                    connectionElements.value.push(line)
+                }
+            },
+            updateConnections: () => {
+                connectionElements.value.forEach((connectionElement) => {
+                    connectionElement.position()
+                })
+            },
         }
     },
 }
