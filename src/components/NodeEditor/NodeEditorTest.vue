60c7a9b8a7ea679797d0491949f348cf845d49ba
Thomas Geissl <tg@2av.de>
2021-09-08 17:13:58 +0200

    added inlets and outlets

--- src/components/NodeEditor/NodeEditorTest.vue
+++ src/components/NodeEditor/NodeEditorTest.vue
@@ -7,17 +7,17 @@
             @mouseup="onMouseUp"
             @mouseleave="onMouseUp"
         >
+            {{ selectedStep?.id }}
             <div
                 v-for="step in adminLayout"
                 :key="step.id"
                 class="
                     step
-                    p-10
+                    p-2
                     rounded-lg
                     border
-                    inline
                     bg-white
-                    flex
+                    flex flex-row
                     justify-center
                     items-center
                     shadow
@@ -26,10 +26,29 @@
                     top: step?.position.y + 'px',
                     left: step?.position.x + 'px',
                 }"
-                @mousedown="selectStep(step)"
+                @mousedown="onMouseDown(step, $event)"
             >
-                {{ steps.find((x) => x.id === step.id).name }}
-                {{ step.id }}
+                <div class="inlets bg-green-200 flex flow-col">
+                    <div
+                        class="bg-yellow-200"
+                        @click="onInletClicked({ stepId: step.id })"
+                    >
+                        in
+                    </div>
+                </div>
+                <div class="node-content flex-1">
+                    {{ steps.find((x) => x.id === step.id).name }}
+                    {{ step.id }}
+                </div>
+                <div class="outlets bg-green-200">
+                    <div
+                        class="bg-yellow-200"
+                        @click="onOutletClicked({ stepId: step.id })"
+                    >
+                        next:
+                        {{ steps.find((x) => x.id === step.id).nextStepId }}
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -37,6 +56,14 @@
 
 <script>
 import { ref } from 'vue'
+import { useStore } from 'vuex'
+import { useState } from '../../composables/state'
+
+const MODES = {
+    NONE: 'NONE',
+    ADD: 'ADD',
+    DELETE: 'DELETE',
+}
 
 export default {
     name: 'NodeEditorTest',
@@ -49,34 +76,40 @@ export default {
     setup(props) {
         const selectedStep = ref(null)
         const adminLayout = ref(
-            props.steps.map((step) => {
+            props.steps.map((step, index) => {
                 return {
                     id: step.id,
                     position: {
-                        x: 200,
+                        x: 100 + index * 240,
                         y: 100,
                     },
                 }
             }),
         )
+        const store = useStore()
+        const [mode, setMode] = useState(MODES.NONE)
+        const [selectedInlet, setSelectedInlet] = useState(null)
+        const [selectedOutlet, setSelectedOutlet] = useState(null)
 
-        const selectStep = (step) => {
+        const onMouseDown = (step, e) => {
+            console.log('onmousedown', step, e)
+            const nodeEditor = document.getElementById('nodeEditor')
+            const nodeEditorRect = nodeEditor.getBoundingClientRect()
             selectedStep.value = step
-            if (!selectedStep.value.position) {
-                selectedStep.value.position = {
-                    x: 10,
-                    y: 10,
-                }
+            step.value.position = {
+                x: e.clientX - nodeEditorRect.left,
+                y: e.clientY - nodeEditorRect.top,
             }
         }
 
         const onMouseMove = (e) => {
+            if (!selectedStep.value) return
             const nodeEditor = document.getElementById('nodeEditor')
             const nodeEditorRect = nodeEditor.getBoundingClientRect()
 
-            if (selectedStep.value) {
-                selectedStep.value.position.x = e.clientX - nodeEditorRect.left
-                selectedStep.value.position.y = e.clientY - nodeEditorRect.top
+            selectedStep.value.position = {
+                x: e.clientX - nodeEditorRect.left,
+                y: e.clientY - nodeEditorRect.top,
             }
         }
 
@@ -84,12 +117,70 @@ export default {
             selectedStep.value = null
         }
 
+        const onInletClicked = (inlet) => {
+            console.log('inlet clicked', inlet)
+            switch (mode.value) {
+                case MODES.ADD: {
+                    setSelectedInlet(inlet)
+                    if (selectedOutlet.value) {
+                        // store.dispatch(
+                        //     'surveys/updateOneSurveyStepAndAddToSelected',
+                        //     {
+                        //         data: {
+                        //             ...selectedOutlet.value.node,
+                        //             nextStepId:
+                        //                 selectedInlet.value.node.id,
+                        //         },
+                        //     },
+                        // )
+                        setSelectedInlet(null)
+                        setSelectedOutlet(null)
+                        setMode(MODES.NONE)
+                    }
+                }
+            }
+        }
+        const onOutletClicked = (outlet) => {
+            console.log('outlet clicked', outlet)
+            switch (mode.value) {
+                case MODES.ADD: {
+                    setSelectedOutlet(outlet)
+                    if (selectedInlet.value) {
+                        console.log(
+                            'TODO: update connection',
+                            selectedInlet,
+                            selectedOutlet,
+                        )
+                        setSelectedInlet(null)
+                        setSelectedOutlet(null)
+                        setMode(MODES.NONE)
+                    }
+                    break
+                }
+                case MODES.DELETE: {
+                    console.log('TODO: remove connection')
+                    // if (outlet.name === 'next') {
+                    // store.dispatch(
+                    //     'surveys/updateOneSurveyStepAndAddToSelected',
+                    //     { data: { ...node, nextStepId: null } },
+                    // )
+                    setMode(MODES.NONE)
+                    break
+                    // }
+                }
+            }
+        }
+
         return {
-            selectStep,
             selectedStep,
+            selectedInlet,
+            selectedOutlet,
+            onMouseDown,
             onMouseMove,
             onMouseUp,
             adminLayout,
+            onInletClicked,
+            onOutletClicked,
         }
     },
 }
