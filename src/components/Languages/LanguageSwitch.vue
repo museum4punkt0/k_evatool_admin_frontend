cf948f5d4817e7ad7c2d9f0c9deb04d95cd426c0
Manuel Herr <mh@2av.de>
2021-10-04 12:28:45 +0200

    added isValid object to Language Switch

--- src/components/Languages/LanguageSwitch.vue
+++ src/components/Languages/LanguageSwitch.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
-        <div class="rounded overflow-hidden flex flex-row bg-green-700">
+        <div class="rounded overflow-hidden flex flex-row">
             <button
                 v-for="language in languages"
                 :key="language.id"
                 class="text-white px-2 py-1 text-sm flex-auto pointer"
-                :class="
-                    activeLanguage.code === language.code
-                        ? 'bg-blue-500'
-                        : 'bg-blue-700'
-                "
+                :class="{
+                    'bg-blue-500':
+                        activeLanguage.code === language.code && isValid.code,
+                    'bg-red-500':
+                        activeLanguage.code === language.code && !isValid.code,
+                    'bg-blue-700':
+                        activeLanguage.code !== language.code && isValid.code,
+                    'bg-red-700':
+                        activeLanguage.code !== language.code && !isValid.code,
+                }"
                 @click="selectLanguage(language)"
             >
                 {{ language.title }}
+                <exclamation-icon
+                    v-if="!isValid"
+                    class="float-right ml-3 h-5 w-5"
+                />
             </button>
         </div>
     </div>
@@ -21,11 +30,17 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
+import { ExclamationIcon } from '@heroicons/vue/outline'
 
 export default {
     name: 'LanguageSwitch',
+    components: { ExclamationIcon },
     props: {
         activeLanguage: {
+            type: Object,
+            default: null,
+        },
+        isValid: {
             type: Object,
             default: null,
         },
