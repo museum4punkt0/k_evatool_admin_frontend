d7b26f0b7c07b0885c15bad3c0fc2a2a4698107b
Thomas Geissl <tg@2av.de>
2021-10-04 10:24:42 +0200

    added question to emoji type

--- src/components/SurveySteps/ElementTypes/ElementTypeEmoji.vue
+++ src/components/SurveySteps/ElementTypes/ElementTypeEmoji.vue
@@ -1,11 +1,18 @@
 <template>
+    <language-switch
+        class="mt-6"
+        :active-language="selectedLanguage"
+        @select="setSelectedLanguage($event)"
+    />
     <form-input
-        v-for="language in store.state.languages.data"
-        :key="'lang' + language.id"
+        v-for="language in store.state.languages.languages.filter(
+            (item) => item.code === selectedLanguage.code,
+        )"
+        :key="'question_lang' + language.id"
         v-model:value="paramsLocal.question[language.code]"
         class="mt-3"
-        :label="'question (' + language.code + ')'"
-        :name="'question_' + language.code"
+        :name="'question' + language.id"
+        :label="t('questions', 1) + ' (' + language.title + ')'"
     />
 
     <div class="table-wrap mt-3">
@@ -46,13 +53,14 @@
 import { computed, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import FormInput from '../../Forms/FormInput.vue'
+import LanguageSwitch from '../../Languages/LanguageSwitch.vue'
 
 import { TrashIcon } from '@heroicons/vue/outline'
 import { useStore } from 'vuex'
 
 export default {
     name: 'ElementTypeEmoji',
-    components: { FormInput, TrashIcon },
+    components: { FormInput, TrashIcon, LanguageSwitch },
     props: {
         params: {
             type: Object,
@@ -67,6 +75,12 @@ export default {
             type: '',
             meaning: '',
         })
+        const selectedLanguage = ref(
+            store.state.languages.languages.find((lang) => lang.default),
+        )
+        const setSelectedLanguage = (language) => {
+            selectedLanguage.value = language
+        }
         const paramsLocal = computed({
             get: () => props.params,
             set: (val) => emit('update:params', val),
@@ -91,6 +105,8 @@ export default {
             t,
             addEmoji,
             deleteEmoji,
+            selectedLanguage,
+            setSelectedLanguage,
         }
     },
 }
