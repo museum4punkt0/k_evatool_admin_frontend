e4a1da9f119b58dd508b44c614730fa7a48b824d
Thomas Geissl <tg@2av.de>
2021-12-02 09:35:19 +0100

    displaying keyphrases, added language switch

--- src/components/Stats/stepResults/TextInputResults.vue
+++ src/components/Stats/stepResults/TextInputResults.vue
@@ -1,19 +1,49 @@
 <template>
     <div>
-        <div
-            v-for="languageCode in Object.keys(results.timespan.results.texts)"
-            :key="languageCode"
-        >
-            {{ languageCode }}
+        <div class="rounded overflow-hidden flex flex-row mb-4">
+            <button
+                v-for="languageCode in Object.keys(
+                    results.timespan.results.analysis,
+                )"
+                :key="languageCode"
+                class="text-white px-2 py-1 text-sm flex-auto pointer"
+                :class="{
+                    primary: selectedLanguage === languageCode,
+                    secondary: selectedLanguage !== languageCode,
+                }"
+                @click="setSelectedLanguage(languageCode)"
+            >
+                {{ languageCode }}
+            </button>
         </div>
-        {{ results.timespan.results.analysis }}
-        <!-- {{ results.total.results.analysis.en.phrases }} -->
-        <!-- <word-cloud :data="results.total.results.analysis.en.phrases"></word-cloud> -->
+        <div>
+            <template
+                v-for="languageCode in Object.keys(
+                    results.timespan.results.analysis,
+                )"
+                :key="languageCode"
+            >
+                <div v-if="languageCode === selectedLanguage">
+                    <div :class="`results_${languageCode}`">
+                        <div
+                            v-for="entry in Object.entries(
+                                results.timespan.results.analysis[languageCode]
+                                    .phrases,
+                            ).sort((a, b) => a[1] < b[1])"
+                            :key="entry[0]"
+                        >
+                            {{ entry[0] }}: {{ entry[1] }}
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
 <script>
 import WordCloud from '../WordCloud.vue'
+import { useState } from '../../../composables/state'
 export default {
     name: 'TextInputResults',
     components: { WordCloud },
@@ -24,7 +54,11 @@ export default {
         },
     },
     setup() {
-        return {}
+        const [selectedLanguage, setSelectedLanguage] = useState(null)
+        return {
+            selectedLanguage,
+            setSelectedLanguage,
+        }
     },
 }
 </script>
