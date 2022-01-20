01122fb02cb2808845cf7a5452afed056906028b
mh <mh@2av.de>
2022-01-20 09:28:12 +0100

    added alias @ = ./src

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pluginEnv from 'vite-plugin-vue-env'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), pluginEnv()],
     server: {
         port: 3002,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 })
