5239fe542d711794a1dc69588391fe04324a0ae4
Thomas Geissl <tg@2av.de>
2022-01-24 09:55:42 +0100

    fixed tendency layout

--- src/components/Stats/SurveyStatsTrendTendency.vue
+++ src/components/Stats/SurveyStatsTrendTendency.vue
@@ -1,24 +1,33 @@
 <template>
     <div
-        class="flex justify-between items-center mb-1 text-sm bg-green-100 text-green-600 rounded-full py-0.5 px-2 ml-2 w-24"
+        class="flex items-center mb-1 text-sm rounded-full p-2"
+        :class="{
+            'text-green-600': percentage > +0,
+            'bg-green-100': percentage >= 0,
+            'text-red-600': percentage < 0,
+            'bg-red-100': percentage < 0,
+        }"
     >
-        <arrow-up-icon />
-        <span>
-            {{ ((1 - current / last) * 100).toFixed(2) }}
-            %
-        </span>
+        <arrow-up-icon v-if="percentage > 0" class="w-5 h-5" />
+        <arrow-down-icon v-else class="w-5 h-5" />
+        <span>{{ percentage }}%</span>
     </div>
 </template>
 
 <script>
-import { ArrowUpIcon } from '@heroicons/vue/outline'
+import { ArrowUpIcon, ArrowDownIcon } from '@heroicons/vue/outline'
 
 export default {
     name: 'SurveyStatsTrendTendency',
-    components: { ArrowUpIcon },
+    components: { ArrowUpIcon, ArrowDownIcon },
     props: {
         current: { type: Number, default: 100 },
         last: { type: Number, default: 200 },
+    },
+    setup(props) {
+        return {
+            percentage: ((1 - props.current / props.last) * 100).toFixed(2),
+        }
     },
 }
 </script>
